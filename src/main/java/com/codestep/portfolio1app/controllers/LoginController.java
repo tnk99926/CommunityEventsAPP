b1d9809a7ca7c152e6ae3d57219e1fe6898938ad
yuu <tnky3704@gmail.com>
2024-06-16 22:53:03 +0900

    feat: ユーザー登録処理を実装

---
+++ src/main/java/com/codestep/portfolio1app/controllers/LoginController.java
@@ -0,0 +1,28 @@
+package com.codestep.portfolio1app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		return mav;
+	}
+	
+	@RequestMapping("/login")
+	public ModelAndView login(ModelAndView mav, @RequestParam(value="error", required=false)String error) {
+		mav.setViewName("login");
+		if (error != null) {
+			mav.addObject("msg", "ログインできませんでした。");
+		} else {
+			mav.addObject("msg","ユーザー名とパスワードを入力");
+		}
+		
+		return mav;
+	}
+}
