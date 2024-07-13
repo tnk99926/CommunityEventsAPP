fba09b8b28876d15bafd0e505b89fee453dc91f4
yuu <tnky3704@gmail.com>
2024-07-13 15:41:13 +0900

    update:県名を	Stringでの受け取りから	Listへ変更

--- src/main/java/com/codestep/portfolio1app/services/ActivityPrefectureService.java
+++ src/main/java/com/codestep/portfolio1app/services/ActivityPrefectureService.java
@@ -1,20 +1,19 @@
 package com.codestep.portfolio1app.services;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 
+import com.codestep.portfolio1app.Exceptions.ValidationException;
 import com.codestep.portfolio1app.entities.ActivityPrefecture;
 import com.codestep.portfolio1app.entities.User;
 import com.codestep.portfolio1app.repositories.ActivityPrefectureRepository;
 import com.codestep.portfolio1app.repositories.PrefectureRepository;
 
-import jakarta.transaction.Transactional;
-
 @Service
-@Transactional
 public class ActivityPrefectureService {
 	
 	@Autowired
@@ -23,18 +22,27 @@ public class ActivityPrefectureService {
 	@Autowired
 	PrefectureRepository prefectureRepository;
 	
+	@Autowired
+    private Validator validator;
+	
 	public List<ActivityPrefecture> getByUser(User user) {
 		return activityPrefectureRepository.getByUser(user);
 	}
-	public void create(User user, String strPrefectures) {
-		int[] prefectures = Stream.of(strPrefectures.split(",")).mapToInt(Integer::parseInt).toArray();
+	public void create(User user, List<ActivityPrefecture> activityPrefectures,BindingResult result) {
 		
-		for(long prefecture: prefectures) {
-			ActivityPrefecture  activityPrefecture = new ActivityPrefecture();
+		for (ActivityPrefecture activityPrefecture : activityPrefectures) {
+			validator.validate(activityPrefecture, result);
+			if (result.hasErrors()) {
+	            throw new ValidationException(result);
+	        }
+		}
+		
+		for(ActivityPrefecture activityPrefecture: activityPrefectures) {
 			activityPrefecture.setUser(user);
-			activityPrefecture.setPrefecture(prefectureRepository.getReferenceById(prefecture));
+			activityPrefecture.setPrefecture(prefectureRepository.getReferenceById(activityPrefecture.getPrefectureId()));
 			activityPrefectureRepository.saveAndFlush(activityPrefecture);
 		}
+		
 		
 	}
 }
