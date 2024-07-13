4f3761722da8ac66bbab2ebece6930189632f4da
yuu <tnky3704@gmail.com>
2024-07-13 15:39:00 +0900

    update:ユーザー情報と活動地域の登録に関する処理を追加

--- src/main/java/com/codestep/portfolio1app/controllers/UserController.java
+++ src/main/java/com/codestep/portfolio1app/controllers/UserController.java
@@ -1,18 +1,23 @@
 package com.codestep.portfolio1app.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.codestep.portfolio1app.Exceptions.ValidationException;
+import com.codestep.portfolio1app.entities.ActivityPrefecture;
 import com.codestep.portfolio1app.entities.Prefecture;
+import com.codestep.portfolio1app.entities.DTO.UserFormDTO;
 import com.codestep.portfolio1app.repositories.PrefectureRepository;
 import com.codestep.portfolio1app.repositories.UserInformationRepository;
 import com.codestep.portfolio1app.repositories.UserRepository;
@@ -20,6 +25,7 @@ import com.codestep.portfolio1app.services.ActivityPrefectureService;
 import com.codestep.portfolio1app.services.UserInformationService;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 @Controller
 public class UserController {
@@ -28,11 +34,10 @@ public class UserController {
 	private PrefectureRepository prefectureRepository;
 	
 	@GetMapping("/user/add")
-	public ModelAndView showAddForm(ModelAndView mav) {
+	public ModelAndView showAddForm(@ModelAttribute("formModel") UserFormDTO userFormDTO, ModelAndView mav) {
 		mav.setViewName("/user/add");
 		List<Prefecture> prefectures = prefectureRepository.findAll();
 		mav.addObject("prefectures", prefectures);
-		
 		return mav;
 	}
 	
@@ -51,23 +56,55 @@ public class UserController {
 	@Autowired
 	UserRepository userRepository;
 	
-	
 	@Transactional
 	@PostMapping("/user/add")
-	public ModelAndView postAddForm(@RequestParam String username, @RequestParam String password, @RequestParam String email,@RequestParam long prefecture,@RequestParam("activity_prefectures") String activityPrefectures, ModelAndView mav) {
-		var user = User
-				.withUsername(username)
-				.password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(password))
-				.roles("USER")
-				.build();
-		userDetailsManager.createUser(user);
-		com.codestep.portfolio1app.entities.User perent = userRepository.getByUsername(username);
+	public ModelAndView postAddForm(@Valid @ModelAttribute("formModel") UserFormDTO userFormDTO, BindingResult result, ModelAndView mav) {
+		if(!result.hasErrors()) {
+			//	ユーザ登録
+			com.codestep.portfolio1app.entities.User formUser = userFormDTO.getUser();
+			
+			if (userDetailsManager.userExists(formUser.getUsername())) {//ユーザ名がすでに存在しないことを確認
+				 List<Prefecture> prefectures = prefectureRepository.findAll();
+					mav.addObject("prefectures", prefectures);
+					mav.addObject("userExistsError", "このユーザー名はすでに使用されています");
+					mav.setViewName("/user/add");
+					return mav;
+	        }
+			var user = User
+					.withUsername(formUser.getUsername())
+					.password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(formUser.getPassword()))
+					.roles("USER")
+					.build();	
+			userDetailsManager.createUser(user);
+			
+			//ユーザ情報登録
+			com.codestep.portfolio1app.entities.User perent = userRepository.getByUsername(user.getUsername());
+			userInformationService.create(perent, userFormDTO.getUserInformation());
+			
+			List<Long> activityPrefectureIds = userFormDTO.getActivityPrefectureIds();
+			List<ActivityPrefecture> activityPrefectures = activityPrefectureIds.stream().map(prefectureid -> new ActivityPrefecture(prefectureid)).collect(Collectors.toList());
+
+			try {
+				if(activityPrefectures != null) {
+					activityPrefectureService.create(perent, activityPrefectures,result);
+				}
+				mav.setViewName("redirect:/");
+				return mav;
+			} catch (ValidationException e) {
+				List<Prefecture> prefectures = prefectureRepository.findAll();
+				mav.addObject("prefectures", prefectures);
+				mav.addObject("activityPrefectureIdsErrors", e.getBindingResult().getFieldError("prefectureId").getDefaultMessage());
+				mav.setViewName("/user/add");
+				return mav;
+			}
+			
+		} else {
+			List<Prefecture> prefectures = prefectureRepository.findAll();
+			mav.addObject("prefectures", prefectures);
+			mav.setViewName("/user/add");
+			return mav;
+		}
 		
-		userInformationService.create(perent, email, prefectureRepository.getReferenceById(prefecture));
-		activityPrefectureService.create(perent, activityPrefectures);
-		
-		mav.setViewName("/user/add");
-		return mav;
 	}
 	
 }
