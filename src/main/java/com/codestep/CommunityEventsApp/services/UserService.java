7f000677a7216199d2feb3d61735a0b0b0f83000
yuu <tnky3704@gmail.com>
2024-08-26 03:07:13 +0900

    update:自作バリデータ使用中止に伴い、メソッド内でユーザー名が存在するかの確認を実施

--- src/main/java/com/codestep/CommunityEventsApp/services/UserService.java
+++ src/main/java/com/codestep/CommunityEventsApp/services/UserService.java
@@ -2,10 +2,13 @@ package com.codestep.CommunityEventsApp.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
 
+import com.codestep.CommunityEventsApp.Exceptions.ValidationException;
 import com.codestep.CommunityEventsApp.repositories.UserRepository;
 
 @Service
@@ -20,12 +23,27 @@ public class UserService {
 		return userRepository.getByUsername(username);
 	}
 	
-	public void create(com.codestep.CommunityEventsApp.entities.User formUser) {
+	public void create(com.codestep.CommunityEventsApp.entities.User formUser,BindingResult result) {
+		if (!userDetailsManager.userExists(formUser.getUsername())) {
+            throw new ValidationException(result);
+        }
+		
 		var user = User
 				.withUsername(formUser.getUsername())
 				.password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(formUser.getPassword()))
 				.roles("USER")
 				.build();	
 		userDetailsManager.createUser(user);
+		
 	}
+	
+	//将来のユーザー名変更用メソッド
+	public void updateName(com.codestep.CommunityEventsApp.entities.User currentUser, com.codestep.CommunityEventsApp.entities.User updatedUser) {
+		UserDetails updateUser = User.withUsername(updatedUser.getUsername())
+									.password(currentUser.getPassword())
+									.roles("USER")
+									.build();	
+		userDetailsManager.createUser(updateUser);
+	}
+	
 }
